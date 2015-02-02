19e61fad4ef8f5ca05bcf3ea9e2da443d9bf3b15
Hao SUN <hasun@zendesk.com>
2015-02-02 10:24:00 -0800

    add settings view

--- TipsCalculator/SettingsViewController.swift
+++ TipsCalculator/SettingsViewController.swift
@@ -2,41 +2,12 @@
 //  SettingsViewController.swift
 //  TipsCalculator
 //
-//  Created by Hao Sun on 1/31/15.
+//  Created by Hao Sun on 2/2/15.
 //  Copyright (c) 2015 Hao Sun. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 
 class SettingsViewController: UIViewController {
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func saveUserSetting(value: Int, key: String) {
-        var defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setInteger(value, forKey: key)
-        defaults.synchronize()
-    }
-    
-    func findUserSetting(key: String) {
-        var defaults = NSUserDefaults.standardUserDefaults()
-        defaults.integerForKey(key)
-    }
-    
-    func tipPercentageKey () {
-        "UserSettingTipPercentage"
-    }
-    
-    @IBAction func saveTipPercentage(sender: AnyObject) {
-        println("called")
-    }
+
 }
