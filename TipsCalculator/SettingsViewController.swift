023a61c7b080511d2e55442227a4ea2d280bd86d
Hao SUN <hasun@zendesk.com>
2015-02-02 12:30:20 -0800

    final

--- TipsCalculator/SettingsViewController.swift
+++ TipsCalculator/SettingsViewController.swift
@@ -6,8 +6,58 @@
 //  Copyright (c) 2015 Hao Sun. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var tip1Field: UITextField!
+    @IBOutlet weak var tip2Field: UITextField!
+    @IBOutlet weak var tip3Field: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var tip1 = Int(loadUserPreference("tip1", defaultValue: 0.18)*100)
+        var tip2 = Int(loadUserPreference("tip2", defaultValue: 0.20)*100)
+        var tip3 = Int(loadUserPreference("tip3", defaultValue: 0.22)*100)
+        tip1Field.text = String(format: "%d", tip1)
+        tip2Field.text = String(format: "%d", tip2)
+        tip3Field.text = String(format: "%d", tip3)
+    }
+    
+    @IBAction func onSettingsDone(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onScreenTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onTip1Changed(sender: AnyObject) {
+        var newtip = NSString(string: tip1Field.text).doubleValue/100
+        saveUserPreference("tip1", value: NSString(string: tip1Field.text).doubleValue/100)
+    }
+    
+    @IBAction func onTip2Changed(sender: AnyObject) {
+        saveUserPreference("tip2", value: NSString(string: tip2Field.text).doubleValue/100)
+    }
+    
+    @IBAction func onTip3Changed(sender: AnyObject) {
+        saveUserPreference("tip3", value: NSString(string: tip3Field.text).doubleValue/100)
+    }
+    
+    func saveUserPreference(key: NSString, value: Double) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(value, forKey: key)
+        defaults.synchronize()
+    }
+    
+    func loadUserPreference(key: NSString, defaultValue: Double) -> Double {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        if defaults.doubleForKey(key) != 0 {
+            return defaults.doubleForKey(key)
+        } else {
+            saveUserPreference(key, value: defaultValue)
+            return defaultValue
+        }
+    }
 }
--- TipsCalculator/ViewController.swift
+++ TipsCalculator/ViewController.swift
@@ -15,11 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    var tipPercentages: Array<Double> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        
+        self.tipPercentages = [
+            loadUserPreference("tip1", defaultValue: 0.18),
+            loadUserPreference("tip2", defaultValue: 0.20),
+            loadUserPreference("tip3", defaultValue: 0.22)
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,17 +36,52 @@ class ViewController: UIViewController {
     }
 
     @IBAction func BillEditingChanged(sender: AnyObject) {
-        var tipPercentages = [ 0.18, 0.20, 0.22 ]
+        refreshValues()
+    }
+
+    @IBAction func onMainTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func saveUserPreference(key: NSString, value: Double) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(value, forKey: key)
+        defaults.synchronize()
+    }
+    
+    func loadUserPreference(key: NSString, defaultValue: Double) -> Double {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        if defaults.doubleForKey(key) != 0 {
+            return defaults.doubleForKey(key)
+        } else {
+            saveUserPreference(key, value: defaultValue)
+            return defaultValue
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tipPercentages = [
+            loadUserPreference("tip1", defaultValue: 0.18),
+            loadUserPreference("tip2", defaultValue: 0.20),
+            loadUserPreference("tip3", defaultValue: 0.22)
+        ]
+        
+        tipControl.setTitle(String(format: "%d%%", Int(tipPercentages[0]*100)), forSegmentAtIndex: 0)
+        tipControl.setTitle(String(format: "%d%%", Int(tipPercentages[1]*100)), forSegmentAtIndex: 1)
+        tipControl.setTitle(String(format: "%d%%", Int(tipPercentages[2]*100)), forSegmentAtIndex: 2)
+        
+        refreshValues()
+    }
+    
+    func refreshValues() {
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         var billAmount = NSString(string: billField.text).doubleValue
         var tip = billAmount * tipPercentage
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", billAmount+tip)
-    }
-
-    @IBAction func onMainTap(sender: AnyObject) {
-        view.endEditing(true)
     }
 }
 
